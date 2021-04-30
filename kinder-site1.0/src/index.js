de6cc331f23635d2ea5d4dc9165b6d8dfa946c6f
Igor <ihor.kravtsov@nure.ua>
2021-04-30 22:24:47 +0300

    добавлен store, Provider и Router

--- kinder-site1.0/src/index.js
+++ kinder-site1.0/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import store from './store';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+      <Router>
+          <App/>
+      </Router>
+    </Provider>,
   document.getElementById('root')
 );
 
